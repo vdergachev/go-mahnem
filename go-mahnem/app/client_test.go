a34eec4302945b89161a3e87d4a405f2e243b52b
Vladimir Dergachev <4dergachev@gmail.com>
2019-09-02 01:20:11 +0300

    Test backbone

---
+++ go-mahnem/app/client_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func TestWebClient_Login(t *testing.T) {
+	// https://github.com/h2non/gock
+}
+
+func TestWebClient_Logout(t *testing.T) {
+	// https://github.com/h2non/gock
+}
+
+func TestWebClient_Profile(t *testing.T) {
+	// https://github.com/h2non/gock
+}
+
+func TestWebClient_Photos(t *testing.T) {
+	// https://github.com/h2non/gock
+}
