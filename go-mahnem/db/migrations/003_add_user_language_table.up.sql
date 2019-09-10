1d84467d6e416f030ddbe8dd1646a4fa2685ee2b
Vladimir Dergachev <vladimir.dergachev@hivehome.com>
2019-09-11 00:41:46 +0300

    Add and use separate database user (#25)
    * Add and use separate database user

--- go-mahnem/db/migrations/003_add_user_language_table.up.sql
+++ go-mahnem/db/migrations/003_add_user_language_table.up.sql
@@ -1,6 +1,10 @@
+BEGIN;
+
 CREATE TABLE IF NOT EXISTS user_language (
     user_language_id SERIAL PRIMARY KEY,
     language_name VARCHAR(50) UNIQUE NOT NULL
 );
 
--- add GRANT ...
+GRANT SELECT, INSERT, UPDATE, DELETE ON public.user_language TO backend;
+
+COMMIT;
